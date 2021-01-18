f5cecde1d919be3be9c99e94c567ba0764315652
e-kov <ikovalenko@assurio.com>
2021-01-18 23:10:34 +0200

    Fix PR comments
    - Check does aws cli installed
    - add possibility to use predefined security group
    - fix message ""SSH into the instance..."

--- scripts/launch-s0-instance.sh
+++ scripts/launch-s0-instance.sh
@@ -3,7 +3,7 @@
 me=$(basename $0)
 dir=$(readlink -f $(dirname $0))
 elastio_aws_id=537513441174
-security_group="elastio-s0-server"
+default_security_group="elastio-s0-server"
 default_instance_type="m5d.2xlarge"
 default_instance_name="$(whoami)-s0-server"
 
@@ -13,30 +13,42 @@ usage()
     echo "   $me --key-name my_ec2_key --name my_s0_server --type m5d.2xlarge"
     echo "   $me -k my_ec2_key -n my_s0_server -t m5ad.2xlarge"
     echo
-    echo "  -k | --key-name : A name of an aws EC2 key in the PEM format for the SSH connection to the instance."
+    echo "  -k | --ssh-key-name       : A name of an aws EC2 key in the PEM format for the SSH connection to the instance."
     echo
-    echo "  -n | --name     : Optional. Name of an instance. It's shown in the AWS console."
-    echo "                    The default value is \"[user_name]-s0-server\"."
+    echo "  -n | --instance-name      : Optional. Name of an instance. It's shown in the AWS console."
+    echo "                              The default value is \"[user_name]-s0-server\"."
     echo
-    echo "  -t | --type     : Optional. An instance type. See https://aws.amazon.com/ec2/instance-types/ for more details."
-    echo "                    The default value is \"m5d.2xlarge\"."
+    echo "  -t | --instance-type      : Optional. An instance type. See https://aws.amazon.com/ec2/instance-types/ for more details."
+    echo "                              The default value is \"m5d.2xlarge\"."
     echo
-    echo "  -h | --help     : Show this usage help."
+    echo "  -g | --security-group     : Optional. A name of the security group which has ports 22 and 61234 open to the world for SSH and s0 server respectively."
+    echo "                              The grop 'elastio-s0-server' will be created and then used in case if this OR next argument is not specified."
+    echo "                              NOTE: The group existance and the pen ports in the group aren't checked."
+    echo "                                    The script may not have permissions and assumes that the group is configured properly."
+    echo
+    echo "  -h | --help             : Show this usage help."
 }
 
 [ "$1" == "" ] && echo "Script need some arguments!" && usage && exit -1
 
 while [ "$1" != "" ]; do
     case $1 in
-        -k | --key-name)   shift && key_name=$1 ;;
-        -n | --name)       shift && instance_name=$1 ;;
-        -t | --type)       shift && instance_type=$1 ;;
-        -h | --help)       usage && exit ;;
-        *)                 echo "Wrong arguments!"
-                           usage && exit 15 ;;
+        -k | --ssh-key-name)        shift && key_name=$1 ;;
+        -n | --instance-name)       shift && instance_name=$1 ;;
+        -t | --instance-type)       shift && instance_type=$1 ;;
+        -g | --security-group)      shift && security_group=$1 ;;
+        -h | --help)                usage && exit ;;
+        *)                          echo "Wrong arguments!"
+                                    usage && exit 15 ;;
     esac
         shift
 done
+
+if ! which aws >/dev/null 2>&1; then
+    echo "Please install aws cli v.2 and try again."
+    echo "You can use a package manager i.e. dnf/yum or apt or follow this instruction https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html."
+    exit 1
+fi
 
 set -e
 
@@ -44,7 +56,7 @@ if [ -z "$key_name" ]; then
     echo "Please specify the '--key-name' parameter. Otherwise, you won't be able to connect to the running instance via ssh!"
     echo
     usage
-    exit 1
+    exit 2
 fi
 
 [ -z "$instance_name" ] && instance_name=$default_instance_name
@@ -54,17 +66,25 @@ latest_ami=$(aws ec2 describe-images --owners $elastio_aws_id \
     --filters "Name=name,Values=s0-ami*" "Name=state,Values=available" \
     --query "reverse(sort_by(Images, &CreationDate))[:1].ImageId" --output text)
 
-if aws ec2 describe-security-groups | jq '.SecurityGroups[].GroupName' | tr -d '"' | grep -q "$security_group" ; then
-    sg_id=$(aws ec2 describe-security-groups --group-names $security_group --query "SecurityGroups[:1].GroupId" --output text)
-else
-    if ! sg_id=$(aws ec2 create-security-group --group-name $security_group --description "Ports 22 and 61234 are open to the world for s0 server and ssh" --output text) ; then
-        echo "Failed to create a security group to open ports for s0 server and ssh. Do you have enough permissions?"
-        exit 2
-    fi
-    if ! aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port 22 --cidr 0.0.0.0/0 ||
-       ! aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port 61234 --cidr 0.0.0.0/0 ; then
-        echo "Failed to open ports 22 and 61234 for s0 server and ssh!"
-        exit 3
+if [ -z "$security_group" ]; then
+    security_group=$default_security_group
+    if seq_gropups=$(aws ec2 describe-security-groups); then
+        if ! echo "$seq_gropups" | jq '.SecurityGroups[].GroupName' | tr -d '"' | grep -q "$security_group" ; then
+            if ! sg_id=$(aws ec2 create-security-group --group-name $security_group --description "Ports 22 and 61234 are open to the world for s0 server and ssh" --output text) ; then
+                echo "Failed to create a security group to open ports for s0 server and ssh. Do you have enough permissions?"
+                exit 3
+            fi
+            if ! aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port 22 --cidr 0.0.0.0/0 ||
+               ! aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port 61234 --cidr 0.0.0.0/0 ; then
+                echo "Failed to open ports 22 and 61234 for s0 server and ssh!"
+                exit 4
+            fi
+        fi
+    else
+        echo "There is no permission to check existance of the security group '$security_group' and get its ID."
+        echo "Please fix permissions to allow operations 'ec2:DescribeSecurityGroups,ec2:DescribeSecurityGroups,ec2:AuthorizeSecurityGroupIngress'"
+        echo "or use script parameter '--security-group' instead!"
+        exit 5
     fi
 fi
 
@@ -92,4 +112,4 @@ instance_dns=$(echo $instance_info | jq ".Reservations[].Instances[].PublicDnsNa
 
 echo "The instance DNS is $instance_dns"
 echo
-echo "SSH into the instance using the key '$key_name' like this: 'ssh -i /path/to/$key_name.pem ec2-user@$instance_dns'"
+echo "SSH into the instance with 'ssh ec2-user@$instance_dns' using key $key_name."
