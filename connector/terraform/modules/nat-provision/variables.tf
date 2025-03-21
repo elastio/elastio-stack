0e90d2393f9bf7065262a054a89a6b015fe0cf59
Veetaha <gersoh3@gmail.com>
2025-03-21 21:39:23 +0000

    Small fix for nat stack

--- connector/terraform/modules/nat-provision/variables.tf
+++ connector/terraform/modules/nat-provision/variables.tf
@@ -25,20 +25,9 @@ variable "connector_account_stack" {
 variable "template_version" {
   description = <<DESCR
     Specifies the version of Elastio NAT provision stack to deploy (e.g. `v5`).
-
-    This is a Cloudformation stack that automatically provisions NAT Gateways in
-    your VPC when Elastio worker instances run to provide them with the outbound
-    Internet access when Elastio is deployed in private subnets.
-
-    If you don't need this stack (e.g. you already have NAT gateways in your VPC
-    or you deploy into public subnets) you can omit this parameter. The default
-    value of `null` means there won't be any NAT provision stack deployed.
-
-    The source code of this stack can be found here:
-    https://github.com/elastio/contrib/tree/master/elastio-nat-provision-lambda
   DESCR
 
   type     = string
-  nullable = true
+  nullable = false
   default  = "v5"
 }
